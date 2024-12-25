9498eebfc44ede7da2cc0bf185051f9adfaac384
Михаил Юрин <iuma@pkzdrav.ru>
2024-12-25 13:45:08 +0700

    Init

---
+++ store/celery.py
@@ -0,0 +1,21 @@
+import os
+
+from celery import Celery
+
+from django.conf import settings
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "store.settings")
+
+app = Celery("store")
+
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.task
+def divide(x: int, y: int) -> float:
+    import time
+    time.sleep(5)
+    return x / y
