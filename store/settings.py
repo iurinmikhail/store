1aefc566e213d701da41eaf82e8c4fa38ec5ac1e
iuma <reggavana56@mail.ru>
2024-12-31 13:42:10 +0700

    feat: Добавлен запуск периодической задачи для очистки сессий

--- store/settings.py
+++ store/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://127.0.0.1:6379/0")
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://127.0.0.1:6379/0")
-CELERY_BEAT_SHEDULE = {
+CELERY_BEAT_SCHEDULE = {
     "task-clear-session": {
         "task": "task_clear_session",
         "schedule": 5.0,  # five seconds
