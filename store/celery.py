db43d6eb992436d7c2c233644e63af343a70c2c7
iuma <reggavana56@mail.ru>
2025-01-11 17:54:37 +0700

    feat: Добавлен сигнал для настроек логирования

--- store/celery.py
+++ store/celery.py
@@ -1,8 +1,14 @@
+import logging
 import os
+from typing import ParamSpec
 
 from celery import Celery
+from celery.app.log import Logging
+from celery.signals import after_setup_logger
 
 from django.conf import settings
+
+P = ParamSpec("P")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "store.settings")
 
@@ -11,3 +17,11 @@ app = Celery("store")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@after_setup_logger.connect()
+def on_after_setup_logger(logger: Logging, *args: P.args, **kwargs: P.kwargs) -> None:  # noqa: ARG001
+    formatter = logger.handlers[0].formatter
+    file_handler = logging.FileHandler("celery.log")
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
